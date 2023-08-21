7ce36369c1d31ae800f4deaf184091cc4628432a
Nosakeys <silaskeys95@gmail.com>
2023-08-21 13:50:07 +0100

    td

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const app = express();
 const authRoutes = require("./routes/authRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 const appointmentRoutes = require("./routes/appointmentRoutes");
-const adminServicesRoutes = require('./routes/adminServicesRoutes')
+const adminRoutes = require('./routes/adminRoutes');
+const reviewRoutes = require('./routes/reviewRoutes');
+const dashboardRoutes = require('./routes/dashboardRoutes');
+const forgotPasswordRoutes = require('./routes/forgotPasswordRoutes');
+const notificationsRoutes = require('./routes/notificationsRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+const analyticsRoutes = require('./routes/analyticsRoutes');
+
 
 //connect db
 mongoose
@@ -22,7 +29,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", authRoutes);
 app.use("/api", profileRoutes);
 app.use("/api", appointmentRoutes);
-app.use("/api",adminServicesRoutes)
+app.use('/api', reviewRoutes);
+app.use('/api', dashboardRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api', forgotPasswordRoutes);
+app.use('/api', notificationsRoutes);
+app.use('/api', searchRoutes);
+app.use('/api', analyticsRoutes);
 
 //start server
 app.listen(process.env.PORT, () => console.log("server started successfully"));
--- models/AdminServices.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AdminServicesSchema = new mongoose.Schema({
-    admin: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin', required: true },
-    service: { type: String, required: true },
-    date: { type: Date, required: true },
-  });
-
-module.exports = mongoose.model('AdminServices', AdminServicesSchema);
